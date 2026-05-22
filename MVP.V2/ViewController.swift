a67384292ac5507477d7b94b6b25000d4f7337c2
Maksim Minakov <maksim.minakov.83@mail.ru>
2026-05-22 20:01:29 +0300

    Initial Commit

---
+++ MVP.V2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVP.V2
+//
+//  Created by Максим Минаков on 22.05.2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
