1d4d0419deae5ac33f2292188aba5574848ccd0c
Maksim Minakov <maksim.minakov.83@mail.ru>
2026-05-25 13:20:23 +0300

    add networkLayer and apiModelData

--- MVP.V2/NetworkDataManager/NetworkDataManager.swift
+++ MVP.V2/NetworkDataManager/NetworkDataManager.swift
@@ -3,36 +3,39 @@ import Alamofire
 
 
 protocol NetworkDataManagerProtocol: AnyObject {
-    func requestData<T: Decodable>(completion: @escaping (Result<T, Error>) -> Void)
+    func requestData<T: Decodable>(completion: @escaping (Result<[T], Error>) -> Void)
 }
 
-class NetworkDataManager {
-    
+class NetworkDataManager: NetworkDataManagerProtocol {
+    func requestData<T>(completion: @escaping (Result<[T], any Error>) -> Void) where T : Decodable {
+        
+        guard let url = URL(string: API.Endpoint.photos.fullURL()) else {
+            completion(.failure(URLError(.badURL)))
+            return
+        }
+        
+        let params: Parameters? = nil
+        AF.request(url, parameters: params)
+            .validate(statusCode: 200..<300)
+            .response { res in
+                // error
+                guard res.error == nil else {
+                    completion(.failure(res.error!))
+                    return
+                }
+                
+                // data
+                guard let data = res.data else {
+                    completion(.failure(URLError(.dataNotAllowed)))
+                    return
+                }
+                
+                do {
+                    let decoded = try JSONDecoder().decode([T].self, from: data)
+                    completion(.success(decoded))
+                } catch {
+                    completion(.failure(error))
+                }
+            }
+    }
 }
-
-
-// "https://jsonplaceholder.typicode.com/photos"
-//[
-//    {
-//    "albumId": 1,
-//    "id": 1,
-//    "title": "accusamus beatae ad facilis cum similique qui sunt",
-//    "url": "https://via.placeholder.com/600/92c952",
-//    "thumbnailUrl": "https://via.placeholder.com/150/92c952"
-//}
-//]
-
-// MARK: - Модель для одного элемента
-struct Photo: Codable {
-    let albumId: Int
-    let id: Int
-    let title: String
-    let url: String
-    let thumbnailUrl: String
-}
-
-// Ваш ответ от сервера - это массив, поэтому используем [Photo]
-typealias PhotosResponse = [Photo]
-
-
-
